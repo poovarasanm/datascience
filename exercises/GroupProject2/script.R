42ca1629eba78f316dbd40ba7dd6c52cfe89c3e7
Poovarasan <poovarasan.murugesan@borngroup.com>
2018-03-24 23:42:18 +0530

    more cleaning

--- exercises/GroupProject2/script.R
+++ exercises/GroupProject2/script.R
@@ -13,6 +13,12 @@ nrow(loan)
 # Business understanding
 
 # Data understanding
+# URL - https://lendingclub.com/browse/loanDetail.action?loan_id=<id>
+# payment_plan - n
+# initial_list_status - f
+# application_type - INDIVIDUAL
+# delinq_amount - 0
+# policy_code - 1
 
 # Data cleaning
 
@@ -27,14 +33,18 @@ loan$emp_length_rounded <- gsub(loan$emp_length_rounded, pattern = '<1', replace
 loan$title <- tolower(loan$title)
 loan$desc <- tolower(loan$desc)
 
+# convert year into length years (4 digits)
+loan$earliest_cr_line <- gsub(loan$earliest_cr_line, pattern="-([0-1])(\\d)$", replacement = "-20\\1\\2")
+loan$earliest_cr_line <- gsub(loan$earliest_cr_line, pattern="-([^0-1])(\\d)$", replacement = "-19\\1\\2")
+
 # Dates
-formatDate <- function (df_variable)
+formatDate <- function (df_variable, format = '%d- %b-%y')
 {
-  return(strptime(paste('1-', df_variable), '%d- %b-%y'))
+  return(strptime(paste('1-', df_variable), format))
 }
 
 loan$issue_d <- formatDate(loan$issue_d)
-loan$earliest_cr_line <- formatDate(loan$earliest_cr_line)
+loan$earliest_cr_line <- formatDate(loan$earliest_cr_line, '%d- %b-%Y')
 loan$last_pymnt_d <- formatDate(loan$last_pymnt_d)
 loan$next_pymnt_d <- formatDate(loan$next_pymnt_d)
 loan$last_credit_pull_d <- formatDate(loan$last_credit_pull_d)
@@ -49,10 +59,11 @@ loan_filtered <- loan[, colSums(is.na(loan)) < no_of_loan_observations]
 
 # remove variables (Findings from summary() results)
 # lc_page_url - equivalent to the load Id
-# payment_plan, initial_list_status - same for all obs
-loan_filtered <- loan_filtered[, !(names(loan_filtered) %in% c('url', 'pymnt_plan', 'initial_list_status'))]
+# payment_plan, initial_list_status, application_type, policy_code - same for all obs
+loan_filtered <- loan_filtered[, !(names(loan_filtered) %in% c('url', 'pymnt_plan', 'initial_list_status',
+                                                               'application_type', 'delinq_amnt', 'policy_code'))]
 
-
+    
 summary(loan_filtered)
 
 # Column correction
@@ -68,7 +79,7 @@ colnames(loan_filtered) <- c('id', 'member_id', 'loan_amount', 'funded_amount', 
                     'total_payment', 'total_payment_investor', 'total_principal_received_amount', 'total_interest_received_amount',
                     'total_late_fee_received', 'recoveries', 'collection_recovery_fee', 'last_payment_date',
                     'last_payment_amount', 'next_payment_date', 'last_credit_pull_date', 'collection_12_months_excluding_medical',
-                    'policy_code', 'application_type', 'acc_now_delinq', 'chargeoff_within_12_months',
-                    'delinq_amount', 'no_of_public_bankruptics', 'tax_lines', 'emp_length_rounded')
+                    'acc_now_delinq', 'chargeoff_within_12_months',
+                    'no_of_public_bankruptics', 'emp_length_rounded')
 
 # remove aggregated/summary columns OR expand them to new columns
