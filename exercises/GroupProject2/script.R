2fffce9a721a851f6c47705a1bfd52fb5703994c
Poovarasan <poovarasan.murugesan@borngroup.com>
2018-03-24 21:29:10 +0530

    test

--- exercises/GroupProject2/script.R
+++ exercises/GroupProject2/script.R
@@ -14,19 +14,6 @@ nrow(loan)
 
 # Data understanding
 
-# Column correction
-# 1. abbrevate the columns
-colnames(loan) <- c('id', 'member_id', 'loan_amount', 'funded_amount', 'funded_amount_investor', 'payment_term',
-                    'int_rate', 'installment_amount', 'grade', 'sub_grade', 'borrower_job_title', 'borrower_employment_length',
-                    'borrower_home_ownership', 'borrower_annual_income', 'income_verification_status', 'issue_date',
-                    'loan_status', 'payment_plan', 'lc_page_url', 'loan_description', 'purpose_category', 'loan_title',
-                    'borrower_zipcode', 'borrower_addr_state', 'debt_to_incom_ratio', 'delinq_2yrs', 'earliest_cr_line',
-                    'inquiries_last_6months', 'months_since_last_delinq', 'months_since_last_public_record',
-                    'open_acc', 'public_rec', 'revolving_balance', 'revolve_line_until_rate', 'total_acc',
-                    'initial_list_status', 'outstanding_principal_amount', 'outstanding_principal_amount_investor',
-                    'total_payment', 'total_payment_investor', 'total_principal_received_amount', 'total_interest_received_amount',
-                    'total_late_fee_received', 'recoveries', 'collection_recovery_fee', 'last_payment_date')...
-
 # Data cleaning
 
 # Characters
@@ -51,5 +38,37 @@ loan$earliest_cr_line <- formatDate(loan$earliest_cr_line)
 loan$last_pymnt_d <- formatDate(loan$last_pymnt_d)
 loan$next_pymnt_d <- formatDate(loan$next_pymnt_d)
 loan$last_credit_pull_d <- formatDate(loan$last_credit_pull_d)
+
+# Numeric formats
+loan$total_rec_late_fee <- as.double(loan$total_rec_late_fee, digits = 2)
+
+# Filtering observations
+no_of_loan_observations <- nrow(loan)
+# remove variable, if all observations are NA
+loan_filtered <- loan[, colSums(is.na(loan)) < no_of_loan_observations]
+
+# remove variables (Findings from summary() results)
+# lc_page_url - equivalent to the load Id
+# payment_plan, initial_list_status - same for all obs
+loan_filtered <- loan_filtered[, !(names(loan_filtered) %in% c('url', 'pymnt_plan', 'initial_list_status'))]
+
+
+summary(loan_filtered)
+
+# Column correction
+# 1. abbrevate the columns
+colnames(loan_filtered) <- c('id', 'member_id', 'loan_amount', 'funded_amount', 'funded_amount_investor', 'payment_term',
+                    'int_rate', 'installment_amount', 'grade', 'sub_grade', 'borrower_job_title', 'borrower_employment_length',
+                    'borrower_home_ownership', 'borrower_annual_income', 'income_verification_status', 'issue_date',
+                    'loan_status', 'loan_description', 'purpose_category', 'loan_title',
+                    'borrower_zipcode', 'borrower_addr_state', 'debt_to_incom_ratio', 'delinq_2yrs', 'earliest_cr_line',
+                    'inquiries_last_6months', 'months_since_last_delinq', 'months_since_last_public_record',
+                    'open_acc', 'public_rec', 'revolving_balance', 'revolve_line_until_rate', 'total_acc',
+                    'outstanding_principal_amount', 'outstanding_principal_amount_investor',
+                    'total_payment', 'total_payment_investor', 'total_principal_received_amount', 'total_interest_received_amount',
+                    'total_late_fee_received', 'recoveries', 'collection_recovery_fee', 'last_payment_date',
+                    'last_payment_amount', 'next_payment_date', 'last_credit_pull_date', 'collection_12_months_excluding_medical',
+                    'policy_code', 'application_type', 'acc_now_delinq', 'chargeoff_within_12_months',
+                    'delinq_amount', 'no_of_public_bankruptics', 'tax_lines', 'emp_length_rounded')
 
 # remove aggregated/summary columns OR expand them to new columns
